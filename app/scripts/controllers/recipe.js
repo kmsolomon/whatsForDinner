0f30a5369200b7a703950e169b4048f8642b7919
kmsolomon <kmsolomon@shaw.ca>
2015-03-03 16:20:48 -0500

    fix so degree symbol shows up properly in recipe details

--- app/scripts/controllers/recipe.js
+++ app/scripts/controllers/recipe.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module('whatsForDinnerApp')
-  .controller('RecipeCtrl', ['$scope', '$routeParams', 'Recipe', function ($scope, $routeParams, Recipe) {
+  .controller('RecipeCtrl', ['$scope', '$routeParams', '$sce' , 'Recipe', function ($scope, $routeParams, $sce, Recipe) {
     
     $scope.recipeId = parseInt($routeParams.recipeId, 10);
 
     $scope.recipe = Recipe.getRecipe($scope.recipeId);
+    
+    $scope.html = $sce.trustAsHtml('&deg;');
 
   }]);
