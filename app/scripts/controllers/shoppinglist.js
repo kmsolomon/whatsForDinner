ebbd38ab399b8ab3ab7594638be201f44acb5605
kmsolomon <kmsolomon@shaw.ca>
2015-06-05 19:18:10 -0400

    fix shared shopping list issue

--- app/scripts/controllers/shoppinglist.js
+++ app/scripts/controllers/shoppinglist.js
@@ -5,6 +5,18 @@ angular.module('whatsForDinnerApp')
     
     $scope.ingredients = {};
     
+    function formatIngredients(){
+      for(var i = 0; i < $scope.ingredients.length; i++) {
+        var tmp = {
+          name: $scope.ingredients[i],
+          checked: false
+        };
+        
+        $scope.ingredients[i] = tmp;
+        
+      }
+    }
+    
     if($routeParams.mon){
       var ids = {
         mon: $routeParams.mon,
@@ -19,24 +31,15 @@ angular.module('whatsForDinnerApp')
       Recipe.searchAllIngredientsFor(ids)
       .success(function(data){
         $scope.ingredients = data;
+        formatIngredients();
       })
       .error(function(data, status){
         console.log(status);
       });
     } else {
       $scope.ingredients = Recipe.getAllIngredients();
+      formatIngredients();
     }
-    
-    for(var i = 0; i < $scope.ingredients.length; i++) {
-      var tmp = {
-        name: $scope.ingredients[i],
-        checked: false
-      };
-      
-      $scope.ingredients[i] = tmp;
-      
-    }
-    
     
     
   }]);
